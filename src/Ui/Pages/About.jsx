f27e7d37e2b9a899c1ac49a55375315b2a5d9f81
jayom <jayomchavda4@gmail.com>
2024-07-05 10:50:42 +0530

    add

--- src/Ui/Pages/About.jsx
+++ src/Ui/Pages/About.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function About() {
     return (
 
-        <div >
+        <div className='bg-[#f9f5f1]' >
             <div className='bg-[#f9f5f1]'>
                 <div>
                     <img src="../../../public/about1.webp" alt="" class="w-full h-auto" />
@@ -50,34 +50,79 @@ export default function About() {
                 </div>
             </div>
 
-            <div className='flex gap-24' >
+            <div className='flex gap-1  mt-9 mb-9   ' >
                 <div>
-                    <img src="../../../public/about2.webp" alt="" className='w-[540px]' />
+                    <img src="../../../public/about2.webp" alt="" className='w-[1750px]' />
                 </div>
 
-                <div className='mx-auto max-w-2xl p-4 text-left'>
-                    <p>Story of</p>
-                    <p>late Haji Ajmal Ali</p>
-                    <p>From a Farmer to a Perfumer</p>
-                    <p>The story of our Founder Father, Late Haji Ajmal Ali is one of the essences of inspiration. Born in a family of rice farmers, hailing from Alinagar – a small village located in Hojai, Assam, India – he realized at a very young age that agriculture would not be able to provide enough to sustain himself and his family.
-                    </p>
-
+                <div className='w-[auto]  text-left mx-auto '>
+                    <i className='title text-[43px]'>Story of</i>
                     <br />
-                    <p>He had heard about the Oudh trade and how it could be a handsome breadwinner for families in his hometown. He decided to chance his fate with the forest, and this decision would lay the foundation for Ajmal.
-                    </p>
-
+                    <i className='title text-[43px]'>late Haji Ajmal Ali</i>
                     <br />
-                    <p>In 1951, he decided to move to Mumbai, India with just Rs 500 as his seed capital and set up his first shop at Nagdevi Street in Crawford Market. The rest, as they say, is history. With honesty, hard work, sincerity and dedication, he slowly and steadily built his business. With his sons also entering the business – within a span of two decades – the family had planted the seeds of the Ajmal Brand and grown them into a strong tree.</p>
+                    <i className='title text-[33px] text-slate-500'>From a Farmer to a Perfumer</i>
+                    <div className='font-Gambetta-Regular text-[18px]'>
+                        <p >The story of our Founder Father, Late Haji Ajmal Ali is one of the essences of inspiration. Born in a family of rice farmers, hailing from Alinagar – a small village located in Hojai, Assam, India – he realized at a very young age that agriculture would not be able to provide enough to sustain himself and his family.
+                        </p>
 
-                    <br />
-                    <p>Thus, a simple Farmer undertook a sterling journey into becoming a world-class Perfumer.</p>
+                        <br />
+                        <p>He had heard about the Oudh trade and how it could be a handsome breadwinner for families in his hometown. He decided to chance his fate with the forest, and this decision would lay the foundation for Ajmal.
+                        </p>
+
+                        <br />
+                        <p>In 1951, he decided to move to Mumbai, India with just Rs 500 as his seed capital and set up his first shop at Nagdevi Street in Crawford Market. The rest, as they say, is history. With honesty, hard work, sincerity and dedication, he slowly and steadily built his business. With his sons also entering the business – within a span of two decades – the family had planted the seeds of the Ajmal Brand and grown them into a strong tree.</p>
+
+                        <br />
+                        <p>Thus, a simple Farmer undertook a sterling journey into becoming a world-class Perfumer.</p>
+                    </div>
 
                 </div>
             </div>
 
+            <div class="relative w-full h-screen overflow-hidden">
+                <div class="absolute inset-0">
+                    <img src="../../../public/about3.webp" alt="Crafting Memories" class="w-full  object-cover" />
+                </div>
+                <div class="absolute top-0 right-0 w-1/2  text-left bg-[#f0ebe2] bg-opacity-0 pt-16 ">
+                    <h1 class=" title text-[40px]  mb-4">Crafting Memories</h1>
+                    <div className='font-Gambetta-Regular text-[18px]'>
+                        <p>At Ajmal Perfumes, we have crafted the art of perfumery into a science – such that we craft everlasting memories through our fragrances.</p>
+                        <br />
+                        <p>Ajmal stands strong as a corporate entity with a vast portfolio of over 300 of the finest and most captivating fragrances. We have established a strong retail presence with over 300+ exclusive "Crafting Memories" retail outlets across the GCC and the world.</p>
+                        <br />
+                        <p>Ajmal also has a presence on the international front; currently exporting our exquisite range of products to 60+ countries across the globe and with an exclusive presence through select 30 global Duty-Free locations and International Airlines.</p>
+                    </div>
+                    <div className='flex text-[15px] mt-10 gap-4'>
+                        <div className='border-[2px]  pt-4 pb-4 pl-4 pr-16 rounded-xl bg-white'>
+                            <p className='font-bold '>Crafting ingredients</p>
+                            <p className='font-Gambetta-Regular'>Ajmal as growers</p>
+                        </div>
+                        <div className='border-[2px]  pt-4 pb-4 pl-4 pr-16 rounded-xl bg-white'>
+                            <p className='font-bold'>Crafting ingredients</p>
+                            <p className='font-Gambetta-Regular'>Ajmal as growers</p>
+                        </div>
+                        <div className='border-[2px]  pt-4 pb-4 pl-4 pr-16 rounded-xl bg-white'>
+                            <p className='font-bold'>Crafting ingredients</p>
+                            <p className='font-Gambetta-Regular'>Ajmal as growers</p>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+
+            <div class="relative ">
+                <div class="absolute inset-0">
+                    <img src="../../../public/about4.webp" alt="" class="w-full " />
+                </div>
+                <div class="absolute top-[230px] left-[300px] text-left font-bold ">
+                    <i class="text-[35px] font-Gambetta-Regular ">Trust needs <br />craftsmanship </i>
+                </div>
+            </div >
 
 
-        </div>
+
+
+        </div >
 
     )
 }
