496a7e4ec87e7216208e433005ace18715d80892
jayom <jayomchavda4@gmail.com>
2024-07-10 11:27:43 +0530

    add

--- src/Ui/Pages/Cart.jsx
+++ src/Ui/Pages/Cart.jsx
@@ -6,6 +6,7 @@ import { useCookies } from "react-cookie";
 
 import { fetchCart, updateCartItem } from "../../Redux/cartSlice";
 import { useEffect } from "react";
+import { Trash } from "react-feather";
 
 export default function Cart() {
     let cartData = useSelector((store) => store.cartSlice);
@@ -87,25 +88,27 @@ export default function Cart() {
         }, 0);
     };
 
+    const calculateTotalItems = () => {
+        return cartData.cart.reduce((total, item) => total + item.count, 0);
+    };
+
+
 
 
 
     return (
         <div>
-            <div className=" bg-gray-100 pt-20  ">
+            <div className=" bg-gray-100 p-5  ">
                 {cartData?.cart?.length > 0 ? (
                     <>
-                        <div className="flex justify-center ">
-                            <div className="flex justify-between ">
-                                <h1 className="mb-10 text-center text-2xl font-bold">
-                                    Cart Items
-                                </h1>
-                                <div>
-                                    <Button onClick={clearCart}>Clear Cart</Button>
-                                </div>
-                            </div>
+                        <div className="flex bg-slate-700  items-center w-full  border-1 border-black rounded-xl py-2" >
+                            <h1 className="text-2xl font-bold flex-grow text-center text-white">
+                                My Cart
+                            </h1>
                         </div>
-                        <div className="mx-auto max-w-5xl justify-center px-6 md:flex md:space-x-6 xl:px-0">
+                        <Button className="ml-auto mt-3 bg-slate-700 p-1" onClick={clearCart}>Clear Cart</Button>
+
+                        <div className="mx-auto max-w-5xl justify-center px-6 md:flex md:space-x-6 xl:px-0 my-3">
                             <div className="rounded-lg">
                                 {cartData?.cart?.map?.((e) => {
                                     return (
@@ -160,21 +163,8 @@ export default function Cart() {
                                                         <p className="text-sm">
                                                             ₹ {e?.count * e?.productId?.price}
                                                         </p>
-                                                        <svg
-                                                            xmlns="http://www.w3.org/2000/svg"
-                                                            fill="none"
-                                                            viewBox="0 0 24 24"
-                                                            strokeWidth="1.5"
-                                                            stroke="currentColor"
-                                                            className="h-5 w-5 cursor-pointer duration-150 hover:text-red-500"
-                                                            onClick={() => removeData(e?.productId?._id)}
-                                                        >
-                                                            <path
-                                                                strokeLinecap="round"
-                                                                strokeLinejoin="round"
-                                                                d="M6 18L18 6M6 6l12 12"
-                                                            />
-                                                        </svg>
+
+                                                        <Trash className="size-5" onClick={() => removeData(e?.productId?._id)} />
                                                     </div>
                                                 </div>
                                             </div>
@@ -182,24 +172,33 @@ export default function Cart() {
                                     );
                                 })}
                             </div>
-                            <div className="mt-6 h-full rounded-lg border bg-white p-6 shadow-md md:mt-0 md:w-1/3">
-                                <div className="mb-2 flex justify-between">
-                                    <p className="text-gray-700">Subtotal</p>
-                                    <p className="text-gray-700">₹{calculateSubtotal().toFixed(2)}</p>
-                                </div>
-                                <div className="flex justify-between">
-                                    <p className="text-gray-700">Shipping</p>
-                                    <p className="text-gray-700">₹4.99</p>
-                                </div>
-                                <hr className="my-4" />
-                                <div className="flex justify-between">
-                                    <p className="text-lg font-bold">Total</p>
-                                    <div className="">
-                                        <p className="mb-1 text-lg font-bold">₹{(calculateSubtotal() + 4.99).toFixed(2)}</p>
-                                        {/* <p className="text-sm text-gray-700">including VAT</p> */}
+                            <div className="mt-6 h-full rounded-lg border-black border-2 bg-[#f3f4f6] p-6 shadow-md md:mt-0 md:w-1/3">
+                                <p className="text-2xl text-left font-bold ">ORDER SUMMERY</p>
+                                <hr className="mb-4" />
+
+                                <div className="bg-white p-3 rounded-2xl">
+                                    <div className="mb-2 flex justify-between">
+                                        <p className="text-gray-700 text-left">{calculateTotalItems()} Products</p>
                                     </div>
+                                    <div className="mb-2 flex justify-between">
+                                        <p className="text-gray-700">Sub-total</p>
+                                        <p className="text-gray-700">₹{calculateSubtotal().toFixed(2)}</p>
+                                    </div>
+                                    <div className="flex justify-between">
+                                        <p className="text-gray-700">Shipping</p>
+                                        <p className="text-gray-700">₹4.99</p>
+                                    </div>
+                                    <hr className="my-4" />
+                                    <div className="flex justify-between">
+                                        <p className="text-lg font-bold "> Order Total</p>
+                                        <div className="">
+                                            <p className="mb-1 text-lg font-bold">₹{(calculateSubtotal() + 4.99).toFixed(2)}</p>
+                                            {/* <p className="text-sm text-gray-700">including VAT</p> */}
+                                        </div>
+                                    </div>
+
                                 </div>
-                                <button className="mt-6 w-full rounded-md bg-blue-500 py-1.5 font-medium text-blue-50 hover:bg-blue-600">
+                                <button className="mt-6 w-full rounded-md  py-1.5 font-medium text-blue-50  bg-slate-700">
                                     Check out
                                 </button>
                             </div>
